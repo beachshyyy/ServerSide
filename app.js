cfcaeb2673e6345e40d7a3796ecce97e97da77ef
beachshyyy <66070132@kmitl.ac.th>
2025-07-31 00:01:12 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const app = express();
 const port = 3000;
 
-const pool = mysql.createPool({
+const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
@@ -12,29 +12,89 @@ const pool = mysql.createPool({
   port: process.env.DB_PORT
 });
 
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('เซิร์ฟเวอร์ทำงานปกติ');
+});
+
 // ดึงสินค้าทั้งหมด
 app.get('/products', (req, res) => {
-  pool.query('SELECT * FROM products', (err, results) => {
-    if (err) return res.status(500).json({ error: err.message });
+  const sql = 'SELECT * FROM products WHERE is_deleted = 0';
+  db.query(sql, (err, results) => {
+    if (err) return res.status(500).send('เกิดข้อผิดพลาด');
     res.json(results);
   });
 });
 
 // ดึงสินค้าตาม ID
 app.get('/products/:id', (req, res) => {
-  pool.query('SELECT * FROM products WHERE id = ?', [req.params.id], (err, results) => {
-    if (err) return res.status(500).json({ error: err.message });
+  const id = req.params.id;
+  const sql = 'SELECT * FROM products WHERE id = ? AND is_deleted = 0';
+  db.query(sql, [id], (err, results) => {
+    if (err) return res.status(500).send('เกิดข้อผิดพลาด');
     res.json(results[0] || {});
   });
 });
 
-// ค้นหาสินค้าตาม keyword
+// ค้นหาสินค้าตามคำค้น
 app.get('/products/search/:keyword', (req, res) => {
   const keyword = `%${req.params.keyword}%`;
-  pool.query('SELECT * FROM products WHERE name LIKE ?', [keyword], (err, results) => {
-    if (err) return res.status(500).json({ error: err.message });
+  const sql = 'SELECT * FROM products WHERE name LIKE ? AND is_deleted = 0';
+  db.query(sql, [keyword], (err, results) => {
+    if (err) return res.status(500).send('เกิดข้อผิดพลาด');
     res.json(results);
   });
+});
+
+
+//เพิ่มสินค้าใหม่
+app.post('/products', (req, res) => {
+    const { name, price, discount, review_count, image_url } = req.body;
+    db.query('INSERT INTO products (name, price, discount, review_count, image_url) VALUES (?, ?, ?, ?, ?)',
+      [name, price, discount, review_count, image_url],
+      (err, results) => {
+        if (err) return res.status(500).json({ error: err.message });
+        res.status(201).json({ id: results.insertId, massage: 'Product created successfully' });
+      }
+    );
+})
+
+//แก้ไขสินค้า
+app.put('/products/:id', (req, res) => {
+  const { name, price, discount, review_count, image_url } = req.body;
+  db.query(
+    'UPDATE products SET name = ?, price = ?, discount = ?, review_count = ?, image_url = ? WHERE id = ?',
+    [name, price, discount, review_count, image_url, req.params.id],
+    (err) => {
+      if (err) return res.status(500).json({ error: err.message });
+      res.json({ message: 'Product update'});
+    }
+  );
+});
+
+//ลบสินค้า SOFT
+app.delete('/products/:id', (req, res) => {
+  db.query(
+    'UPDATE products SET is_deleted = 1 WHERE id = ?',
+    [req.params.id],
+    (err) => {
+      if (err) return res.status(500).json({ error: err.message });
+      res.json({ message: 'Product soft-deleted' });
+    }
+  );
+});
+
+//API กู้คืนข้อมูลที่ถูกลบ
+app.put('/products/restore/:id', (req, res) => {
+  db.query(
+    'UPDATE products SET is_deleted = 0 WHERE id = ?',
+    [req.params.id],
+    (err) => {
+      if (err) return res.status(500).json({ error: err.message });
+      res.json({ message: 'Product restored'});
+    }
+  );
 });
 
 app.listen(port, () => {
