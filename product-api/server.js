0c5a6a6f49a8da4f1879031844a75c59a8ebbfab
beachshyyy <66070132@kmitl.ac.th>
2025-08-14 23:05:31 +0700

    finish

--- product-api/server.js
+++ product-api/server.js
@@ -1,54 +1,25 @@
-const express = require("express")
-const swaggerUi = require("swagger-ui-express")
-const swaggerJsdoc = require("swagger-jsdoc")
+const express = require('express');
+const app = express();
+const PORT = 3000;
 
-const swaggerOptions = {
-    swaggerDefinition: {
-        openapi: "3.0.0",
-        info: {
-            title: "User API",
-            version: "1.0.0",
-            description: "User API information",
-        },
-    },
+const productRoutes = require('./routes/productRoutes');
+const webRoutes = require('./routes/webRoutes');
 
-    servers: [
-        {
-            url: "http://localhost:3000"
-        },
-    ],
 
-    apis: ["server.js"],
-}
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./config/swagger');
+const path = require('path');
 
-const swaggerDocs = swaggerJsdoc(swaggerOptions)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use('/', webRoutes);
+app.use('/api', productRoutes);
 
-const app = express()
+// ใช้งาน Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-app.use(express.json())
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
-
-let users = [
-    {id: 1, name: "A"},
-    {id: 2, name: "B"}
-]
-
-/***
- * @swagger
- * /users:
- *  get:
- *      description: Get all users
- *      responses:
- *          200:
- *              description: Success
- *          400:
- *              description: Error
-*/
-
-app.get("/users", (req, res) => {
-    res.json(users)
-})
-
-app.listen(3000, () => {
-    console.log("server running on http://localhost:3000")
-})
+app.listen(PORT, () => {
+    console.log(`🚀 Server is running at http://localhost:${PORT}`);
+    console.log(`📚 API documentation is available at http://localhost:${PORT}/api-docs`);
+});
